615ae2b39fb77f17ab52d0fd1a28dff156351e9f
Ekardzal <ekardzal@hs-mittweida.de>
2025-02-20 04:14:26 +0100

    Update - Game Concept Moodboard

--- js/toggleError.js
+++ js/toggleError.js
@@ -1,13 +1,13 @@
 function toggleError(button) {
-			if ( button.className === 'buttinvi2' ) {
-				// HIDE ERROR
-				button.className = 'buttinvi1';
+			if ( button.className === 'eyeClosed' ) {
+				// HIDE
+				button.className = 'eyeOpen';
 				element = document.querySelector('.bannerright');
 				element.style.right = '-8%';
 				document.getElementById('icon').src = 'images/icon_eye_closed.svg';
 			} else {
-				// SHOW ERROR
-				button.className = 'buttinvi2';
+				// SHOW
+				button.className = 'eyeClosed';
 				element = document.querySelector('.bannerright');
 				element.style.right = '0%';
 				document.getElementById('icon').src = 'images/icon_eye_open.svg';
